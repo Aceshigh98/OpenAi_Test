cc72da23b6af2154c321faa69e91881c0b9d4b04
AcesHigh <cothermanbrannon@gmail.com>
2024-09-30 11:09:48 -0500

    Pushing to feature branch

---
+++ Configurations/OpenAiConfig.cs
@@ -0,0 +1,8 @@
+
+namespace Cgpt.Configurations
+{
+    public class OpenAiConfig
+    {
+        public string Key { get; set; }
+    }
+}
--- Models/OpenAiRequest.cs
+++ Models/OpenAiRequest.cs
@@ -1,1 +1,31 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace Cgpt.Models
+{
+    public class OpenAiModels
+    {
+        [Required(ErrorMessage = "Model is required")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Prompt is required")]
+        public string Prompt { get; set; }
+
+        [Range(1, 100, ErrorMessage = "MaxTokens must be between 1 and 100")]
+        public int MaxTokens { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Temperature must be between 0 and 10")]
+        public double Temperature { get; set; }
+
+        public OpenAiModel(string model, string prompt)
+        {
+            Model = model;
+            Prompt = prompt;
+        }
+    }
+
+    public class OpenAiResponse
+    {
+        [Required(ErrorMessage = "Completion is required")]
+        public string Completion { get; set; }
+    }
+}
